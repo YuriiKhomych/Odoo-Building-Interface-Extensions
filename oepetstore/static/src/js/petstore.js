2edfd7aced74ec65bc0e379d11954b1eee71e843
yuriikhomych <yuriykhomich@gmail.com>
2018-02-16 15:19:14 +0200

    [IMP] QWeb Context

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -10,19 +10,18 @@ odoo.define('oepetstore.petstore', function (require) {
 
     var homePage = Widget.extend({
         template: "HomePageTemplate",
-        className: 'oe_petstore_homepage',
         init: function(parent) {
             this._super(parent);
+            this.name = "Mordecai";
             console.log("Hello JS, I'm inside of init.");
         },
         start: function() {
             console.log("Your pet store home page loaded");
-            this.$el.append("<div>Hello dear Odoo user!</div>");
+
             var greeting = new GreetingsWidget(this);
             greeting.appendTo(this.$el);
-            console.log(this.getChildren()[0].$el);
             // will print "div.oe_petstore_greetings" in the console
-            return greeting.appendTo(this.$el);
+
         },
     });
     core.action_registry.add('petstore.homepage', homePage);
@@ -34,9 +33,9 @@ odoo.define('oepetstore.petstore', function (require) {
             this.name = name;
         },
         start: function() {
+
             this.$el.append("<div>We are so happy to see you again in this menu!</div>");
-            console.log(this.getParent().$el );
-            // will print "div.oe_petstore_homepage" in the console
+
         },
     });
 
