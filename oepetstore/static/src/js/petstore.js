9575262df6ce92098e2eee79b20538af8881d0eb
yuriikhomych <yuriykhomich@gmail.com>
2018-02-16 16:27:33 +0200

    [Exercise] Widget Events

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -13,17 +13,43 @@ odoo.define('oepetstore.petstore', function (require) {
             var products = new ProductsWidget(
                 this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
             products.appendTo(this.$el);
-            },
-    });
-
-    var ProductsWidget = Widget.extend({
-        template: "ProductsWidget",
-        init: function(parent, products, color) {
-            this._super(parent);
-            this.products = products;
-            this.color = color;
+            var widget = new ConfirmWidget(this);
+            widget.on("user_chose", this, this.user_chose);
+            widget.appendTo(this.$el);
         },
-    });
-    core.action_registry.add('petstore.homepage', homePage);
+        user_chose: function(confirm) {
+            if (confirm) {
+                console.log("The user agreed to continue");
+            } else {
+                console.log("The user refused to continue");
+            }
+        },
+});
+
+var ProductsWidget = Widget.extend({
+    template: "ProductsWidget",
+    init: function(parent, products, color) {
+        this._super(parent);
+        this.products = products;
+        this.color = color;
+    },
+});
+
+var ConfirmWidget = Widget.extend({
+    events: {
+        'click button.ok_button': function () {
+            this.trigger('user_chose', true);
+        },
+        'click button.cancel_button': function () {
+            this.trigger('user_chose', false);
+        }
+    },
+    start: function() {
+        this.$el.append("<div>Are you sure you want to perform this action?</div>" +
+            "<button class='ok_button'>Ok</button>" +
+            "<button class='cancel_button'>Cancel</button>");
+    },
+});
+core.action_registry.add('petstore.homepage', homePage);
 
 });
