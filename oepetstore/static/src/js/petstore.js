09934d7b2fbd1b6109bdf7b9895292413cec5d41
yuriikhomych <yuriykhomich@gmail.com>
2018-02-16 14:45:03 +0200

    [IMP] Write first exercices from start to QWeb

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -1,13 +1,43 @@
-openerp.oepetstore = function(instance, local) {
-    var _t = instance.web._t,
-        _lt = instance.web._lt;
-    var QWeb = instance.web.qweb;
+odoo.define('oepetstore.petstore', function (require) {
+    "use strict";
+    var Class = require('web.Class');
+    var Widget = require('web.Widget');
+    var core = require('web.core');
+    var utils = require('web.utils');
+    var QWeb = core.qweb;
+    var _t = core._t;
+    var _lt = core._lt;
 
-    local.HomePage = instance.Widget.extend({
+    var homePage = Widget.extend({
+        className: 'oe_petstore_homepage',
+        init: function(parent) {
+            this._super(parent);
+            console.log("Hello JS, I'm inside of init.");
+        },
         start: function() {
-            console.log("pet store home page loaded");
+            console.log("Your pet store home page loaded");
+            this.$el.append("<div>Hello dear Odoo user!</div>");
+            this.$el.append(QWeb.render("HomePageTemplate"));
+            var greeting = new GreetingsWidget(this);
+            greeting.appendTo(this.$el);
+            console.log(this.getChildren()[0].$el);
+            // will print "div.oe_petstore_greetings" in the console
+            return greeting.appendTo(this.$el);
+        },
+    });
+    core.action_registry.add('petstore.homepage', homePage);
+
+    var GreetingsWidget = Widget.extend({
+        className: 'oe_petstore_greetings',
+        init: function(parent, name) {
+            this._super(parent);
+            this.name = name;
+        },
+        start: function() {
+            this.$el.append("<div>We are so happy to see you again in this menu!</div>");
+            console.log(this.getParent().$el );
+            // will print "div.oe_petstore_homepage" in the console
         },
     });
 
-    instance.web.client_actions.add('petstore.homepage', 'instance.oepetstore.HomePage');
-}
+});
