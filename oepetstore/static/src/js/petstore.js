9aac5c820d29c1081bbd09a3fdedc8abb1d5050e
yuriikhomych <yuriykhomich@gmail.com>
2018-07-18 11:12:02 +0300

    [Exercise] Usage of QWeb in Widgets

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -9,37 +9,21 @@ odoo.define('oepetstore.petstore', function (require) {
     var _lt = core._lt;
 
     var homePage = Widget.extend({
-        template: "HomePageTemplate",
-        className: 'oe_petstore_homepage',
-        init: function(parent) {
-            this._super(parent);
-            this.name = "Mordecai";
-            console.log("Hello JS, I'm inside of init.");
-        },
         start: function() {
-            console.log("Your pet store home page loaded");
-            this.$el.append("<div>Hello dear Odoo user!</div>");
-            var greeting = new GreetingsWidget(this);
-            greeting.appendTo(this.$el);
-            console.log(this.getChildren()[0].$el);
-            // will print "div.oe_petstore_greetings" in the console
-            // this.$el.append(QWeb.render("HomePageTemplate", {name: "Klaus"}));
-            return greeting.appendTo(this.$el);
+            var products = new ProductsWidget(
+                this, ["cpu", "mouse", "keyboard", "graphic card", "screen"], "#00FF00");
+            products.appendTo(this.$el);
+            },
+    });
+
+    var ProductsWidget = Widget.extend({
+        template: "ProductsWidget",
+        init: function(parent, products, color) {
+            this._super(parent);
+            this.products = products;
+            this.color = color;
         },
     });
     core.action_registry.add('petstore.homepage', homePage);
-
-    var GreetingsWidget = Widget.extend({
-        className: 'oe_petstore_greetings',
-        init: function(parent, name) {
-            this._super(parent);
-            this.name = name;
-        },
-        start: function() {
-            this.$el.append("<div>We are so happy to see you again in this menu!</div>");
-            console.log(this.getParent().$el );
-            // will print "div.oe_petstore_homepage" in the console
-        },
-    });
 
 });
